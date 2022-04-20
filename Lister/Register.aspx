6652302866c31aede16d86bca16bc67638875cea
Craig Miller <millercj6@jacks.sfasu.edu>
2022-04-20 10:29:31 -0500

    update 1.1

---
+++ Lister/Register.aspx
@@ -0,0 +1,13 @@
+﻿ <%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="Lister.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="form-group">
+        <label for="txtEmail">Email</label>
+        <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" ClintIDMode="Static"></asp:TextBox>
+        <label for="txtPassword">Password</label>
+        <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" ClintIDMode="Static" TextMode="Password"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <asp:Button ID="btnRegister" runat="server" CssClass="btn btn-primary" Text="Register" />
+        <label ID="lblRegisterFailed" runat="server" CssClass="alert alert-danger" Visible="false"></label>
+    </div>
+</asp:Content>
---
+++ Lister/Register.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.Security;
+
+namespace Lister
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        { 
+            if(Page.IsValid)
+            {
+                string email = txtEmail.Text.Trim();
+                string password = txtPassword.Text.Trim();
+
+                if(!IsEmailRegistered(email))
+                {
+                    RegisterUser(email, password);
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void RegisterUser(string email, string password)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["ListerConnectionString"].ConnectionString;
+#pragma warning disable CS0618 // Type or member is obsolete
+                string hashedPassword = FormsAuthentication.HashPasswordForStoringInConfigFile(password, "SHA1");
+#pragma warning restore CS0618 // Type or member is obsolete
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO Users(Email, HashedPassword) VALUES('" + email + "', '" + hashedPassword + "')"; ;
+                cmd.Connection = conn;
+                conn.Open();
+
+                if(cmd.ExecuteNonQuery() == 1)
+                {
+                    Response.Redirect("Signin.aspx");
+                }
+                else
+                {
+                    lblRegisterFailed.Visible = true;
+                    lblRegisterFailed.InnerText = "Register Failed, Try again.";
+                }
+            }
+
+        }
+
+        private bool IsEmailRegistered(string email)
+        {
+            using(SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["ListerConnectionString"].ConnectionString;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT * FROM Users WHERE Email = '" + email + "'";
+                cmd.Connection = conn;
+                conn.Open();
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if(sdr.HasRows)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+}
